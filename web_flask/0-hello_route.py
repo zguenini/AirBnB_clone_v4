2a28ee2c29298f71f3985978ca7e6925bb6b5ed6
zguenini <133226252+zguenini@users.noreply.github.com>
2024-07-13 11:24:43 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
